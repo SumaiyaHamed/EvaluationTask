7812588716001c1056ad989aee6b853fb36fdf80
SumaiyaHamed <sumaiyahit@gmail.com>
2022-11-29 11:58:34 +0400

    search from file a string which is user input in java

--- src/Evaluation/Main.java
+++ src/Evaluation/Main.java
@@ -1,6 +1,7 @@
 package Evaluation;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -14,11 +15,9 @@ import java.util.Scanner;
 
 import com.google.gson.Gson;
 
-
-
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.nationalize.io/?name=nathanial"))
 				.method("GET", HttpRequest.BodyPublishers.noBody()).build();
 		HttpResponse<String> response = null;
@@ -31,42 +30,44 @@ public class Main {
 		}
 
 		System.out.println(response.body());
-		String task=response.body();
+		String task = response.body();
 		System.out.println("  ");
-		
+
 //Write response in a file:
 		System.out.println("****************************************************************");
 		System.out.println("  ");
 		try {
 
-		FileWriter filewriter = new FileWriter("evaluationfile.txt");
-		filewriter.write(task.toString());
-		filewriter.close();
-		
+			FileWriter filewriter = new FileWriter("evaluationfile.txt");
+			filewriter.write(task.toString());
+			filewriter.close();
 
-		FileReader read = new FileReader("evaluationfile.txt");
-		BufferedReader buffeRedreader=new BufferedReader(read);
-		String line;
-		while((line=buffeRedreader.readLine())!=null) {
-			System.out.println(line);
-		}read.close();
-		}catch (IOException e) {
+			FileReader read = new FileReader("evaluationfile.txt");
+			BufferedReader buffeRedreader = new BufferedReader(read);
+			String line;
+			while ((line = buffeRedreader.readLine()) != null) {
+				System.out.println(line);
+			}
+			read.close();
+		} catch (IOException e) {
 			e.printStackTrace();
-		} 
+		}
 		System.out.println("_____________");
 		System.out.println("succuss....");
 		System.out.println("_____________");
 		System.out.println("  ");
 		System.out.println("****************************************************************");
 		System.out.println("  ");
-		
-		//search from file a string which is user input in java:
+
+		// search from file a string which is user input in java:
 		Scanner sa=new Scanner(System.in);
 		System.out.println("Enter the word to be found");
 		String word=sa.next();
 		boolean result=false;
 		int count=0;
-		System.out.println("contentof the line");
+		
+		System.out.println("content of the line");
+		
 		Scanner sa1=new Scanner(new FileInputStream("C:\\Users\\User007\\eclipse-workspace\\EvaluationTask\\evaluationfile.txt"));
 		while(sa1.hasNextLine()) {
 			String line=sa1.nextLine();
@@ -80,14 +81,14 @@ public class Main {
 			System.out.println("number is:"+count);
 	
 		}else {
-			System.out.println("nathing:");
+			System.out.println("The word that you want to found is not given");
 		}
-		
-		
-		
-		//Gson gson = new Gson();
 
-	  // TopApi topapi = gson.fromJson(response.body().toString(), TopApi.class);
+		
+
+		// Gson gson = new Gson();
+
+		// TopApi topapi = gson.fromJson(response.body().toString(), TopApi.class);
 
 	}
 
